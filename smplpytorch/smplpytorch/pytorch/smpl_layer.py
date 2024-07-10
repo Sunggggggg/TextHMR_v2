8b92ceee02f5502b122a6d9067114e969a49907d
Sunggggggg <sky990319@gmail.com>
2024-07-10 13:56:34 +0900

    f

--- smplpytorch/smplpytorch/pytorch/smpl_layer.py
+++ smplpytorch/smplpytorch/pytorch/smpl_layer.py
@@ -4,9 +4,9 @@ import numpy as np
 import torch
 from torch.nn import Module
 
-from smplpytorch.native.webuser.serialization import ready_arguments
-from smplpytorch.pytorch import rodrigues_layer
-from smplpytorch.pytorch.tensutils import (th_posemap_axisang, th_with_zeros, th_pack, make_list, subtract_flat_id)
+from smplpytorch.smplpytorch.native.webuser.serialization import ready_arguments
+from smplpytorch.smplpytorch.pytorch import rodrigues_layer
+from smplpytorch.smplpytorch.pytorch.tensutils import (th_posemap_axisang, th_with_zeros, th_pack, make_list, subtract_flat_id)
 
 
 class SMPL_Layer(Module):
