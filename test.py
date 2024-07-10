c8a2eb913e96a8bc548ba9b78c7ead365e54ae76
Sunggggggg <sky990319@gmail.com>
2024-07-10 13:45:15 +0900

    f

---
+++ test.py
@@ -0,0 +1,19 @@
+import argparse
+from lib.core.base import get_dataloader
+from lib.core.config import cfg, update_config
+
+parser = argparse.ArgumentParser(description='Train Pose2Mesh')
+
+parser.add_argument('--seed', type=int, default=123, help='random seed to use. Default=123')
+parser.add_argument('--resume_training', action='store_true', help='Resume Training')
+parser.add_argument('--debug', action='store_true', help='reduce dataset items')
+parser.add_argument('--gpu', type=str, default='0,1', help='assign multi-gpus by comma concat')
+parser.add_argument('--cfg', type=str, help='experiment configure file name')
+
+args = parser.parse_args()
+if args.cfg:
+    update_config(args.cfg)
+
+dataset_names = ['Human36M', 'MPII3D', 'PW3D']
+is_train = True
+get_dataloader(args, dataset_names, is_train)
