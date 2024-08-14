c662c3e06230fd9b66699c649ac7d6d1b9d090a0
Sunggggggg <sky990319@gmail.com>
2024-08-14 21:47:59 +0900

    f

--- lib/core/config.py
+++ lib/core/config.py
@@ -57,8 +57,7 @@ cfg.DATASET.BASE_DATA_DIR = 'data/base_data'
 """ Model """
 cfg.MODEL = edict()
 cfg.MODEL.name = 'PMCE'
-cfg.MODEL.hpe_dim = 256
-cfg.MODEL.hpe_dep = 3
+cfg.MODEL.embed_dim = 256
 cfg.MODEL.joint_dim = 64
 cfg.MODEL.vertx_dim = 64
 cfg.MODEL.input_shape = (384, 288)
